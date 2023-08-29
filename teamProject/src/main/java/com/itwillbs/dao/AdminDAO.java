8bb7186a49316a2a228fc5ee7a0a40c34435b353
kkm <kkm@kkm54>
2023-08-29 09:03:20 +0900

    dso test

--- teamProject/src/main/java/com/itwillbs/dao/AdminDAO.java
+++ teamProject/src/main/java/com/itwillbs/dao/AdminDAO.java
@@ -1,5 +1,47 @@
 package com.itwillbs.dao;
 
-public class AdminDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class AdminDAO {
+	Connection con=null;
+	PreparedStatement pstmt=null;
+	ResultSet rs =null;
+	
+	//1,2 단계 디비 연결 메서드  정의 => 필요로 할때 호출 사용
+	public Connection getConnection() throws Exception {
+		Context init = new InitialContext();
+		DataSource ds=
+		(DataSource)init.lookup("java:comp/env/jdbc/c1d2304t4");
+		con=ds.getConnection();
+		return con;
+	}
+	//기억장소 해제 메서드()
+	public void dbClose() {
+		//  => con, pstmt, rs 기억장소 해제
+		if(rs != null) {try {rs.close();} catch (SQLException e) {	}}			
+		if(pstmt != null) {try {pstmt.close();} catch (SQLException e) {	}}
+		if(con != null) {try {con.close();} catch (SQLException e) {	}}
+	}
+	public void testSave(int num, String id) {
+		try {
+			con = this.getConnection();
+			String sql = "insert into test123 values (?, ?)";
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setInt(1, num);
+			pstmt.setString(2, id);
+			pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			this.dbClose();
+		}
+	}
 }
