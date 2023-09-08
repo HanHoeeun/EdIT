df7162e4763ae178383270645743302f635c2332
hoeeunhan <hoeeunhan@192.168.0.8>
2023-09-09 02:18:38 +0900

    풀 하시고 위시리스트 잘 되는지 확인 한번만 해주세요........

--- teamProject/src/main/java/com/itwillbs/dao/ProductDAO.java
+++ teamProject/src/main/java/com/itwillbs/dao/ProductDAO.java
@@ -1642,7 +1642,7 @@ public class ProductDAO {
 		//int size = wishList.size();
 		try {
 			con = getConnection();
-			String sql = "SELECT w.w_num, m.m_nick, p.p_file, p.p_title, p.p_status, p.p_type, p.p_price "
+			String sql = "SELECT w.w_num, m.m_nick, p.p_file, p.p_title, p.p_status, p.p_type, p.p_price, p.p_num "
 					+ "from wishlists w "
 					+ "JOIN products p ON w.w_p_num = p.p_num "
 					+ "JOIN members m ON w.w_m_num = m.m_num "
@@ -1669,6 +1669,7 @@ public class ProductDAO {
 	            productDTO.setP_status(rs.getString("p_status"));
 	            productDTO.setP_type(rs.getString("p_type"));
 	            productDTO.setP_price(rs.getInt("p_price"));
+	            productDTO.setP_num(rs.getInt("p_num"));
 
 	            wishListDTO.setMemberDTO(memberDTO);
 	            wishListDTO.setProductDTO(productDTO);
